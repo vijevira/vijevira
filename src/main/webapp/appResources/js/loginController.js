a803de7282118db1fdb235163da2a7f837cdfb21
Vijendra Kumar <vijevira@gmail.com>
2022-06-15 15:14:49 +0530

    Update loginController.js

--- src/main/webapp/appResources/js/loginController.js
+++ src/main/webapp/appResources/js/loginController.js
@@ -142,13 +142,7 @@ function signup(){
 		const { spawn } = require('child_process');
 const temperatures = []; // Store readings
 
-const sensor = spawn('python', ['sensor.py']);
-sensor.stdout.on('data', function(data) {
-
-    // convert Buffer object to Float
-    temperatures.push(parseFloat(data));
-    console.log(temperatures);
-});                                        
+                                      
    console.log("success");
 
 	}else{
